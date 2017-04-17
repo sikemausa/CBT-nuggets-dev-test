9b324583346ed25a1e3642e39417057971a1283b
Mike Sausa <mikejsausa@gmail.com>
2017-04-16 21:46:58 -0600

    production

--- tools/publicServer.js
+++ tools/publicServer.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var open = require('open');
 var compression = require('compression');
-var favicon = require('serve-favicon');
 
 /*eslint-disable no-console */
 
